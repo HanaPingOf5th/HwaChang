b7d731b2e561a33167a542b4cb42a329c0a7de17
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-10-24 19:30:55 +0900

    feat: 상담하러 가기 page 추가

---
+++ web/src/app/(..route)/teller/prepare/page.tsx
@@ -0,0 +1,12 @@
+import Content from "@/app/ui/teller-room/content";
+import InfoBar from "@/app/ui/teller-room/info-bar";
+import React from "react";
+
+export default function PrepareCounsel() {
+  return (
+    <div>
+      <InfoBar />
+      <Content />
+    </div>
+  )
+}
