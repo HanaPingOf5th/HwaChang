ce19a895d74f5cbad6c012aa0ea75c7f1096db77
yoounyoungheon <iddyoon@gmail.com>
2024-10-25 10:12:53 +0900

    feat: 고객 카테고리 - 쿼리파라미터에 따른 분기처리

---
+++ web/src/app/(..route)/customer/main/enterance/categories.ts
@@ -0,0 +1,107 @@
+import { FaMoneyBill } from "react-icons/fa";
+import { IconType } from "react-icons/lib";
+interface category{
+  title: string;
+  icon: IconType;
+}
+export const customerCategories:category[] = [
+  {
+    title: '예금',
+    icon: FaMoneyBill
+  },
+  {
+    title: '펀드/신택',
+    icon: FaMoneyBill
+  },
+  {
+    title: '카드',
+    icon: FaMoneyBill
+  },
+  {
+    title: '보험',
+    icon: FaMoneyBill
+  },
+  {
+    title: '대출',
+    icon: FaMoneyBill
+  },
+  {
+    title: '스마트 뱅킹',
+    icon: FaMoneyBill
+  },
+  {
+    title: '인증서',
+    icon: FaMoneyBill
+  },
+  {
+    title: '주택 청약',
+    icon: FaMoneyBill
+  },
+  {
+    title: '텔레뱅킹',
+    icon: FaMoneyBill
+  },
+  {
+    title: '금융 사기',
+    icon: FaMoneyBill
+  },
+  {
+    title: '자동 이체',
+    icon: FaMoneyBill
+  },
+  {
+    title: '기타',
+    icon: FaMoneyBill
+  }
+]
+
+export const enterpriseCategories = [
+  {
+    title: '대출',
+    icon: FaMoneyBill
+  },
+  {
+    title: '외환',
+    icon: FaMoneyBill
+  },
+  {
+    title: '입출금 알림',
+    icon: FaMoneyBill
+  },
+  {
+    title: '펀드/신탁',
+    icon: FaMoneyBill
+  },
+  {
+    title: '오픈 뱅킹',
+    icon: FaMoneyBill
+  },
+  {
+    title: '예금',
+    icon: FaMoneyBill
+  },
+  {
+    title: '자동이체',
+    icon: FaMoneyBill
+  },
+  {
+    title: '금융 사기',
+    icon: FaMoneyBill
+  },
+  {
+    title: '텔레뱅킹',
+    icon: FaMoneyBill
+  },
+  {
+    title: '인증서',
+    icon: FaMoneyBill
+  },
+  {
+    title: '우수 고객',
+    icon: FaMoneyBill
+  },
+  {
+    title: '기타',
+    icon: FaMoneyBill
+  }
+]
--- web/src/app/(..route)/customer/main/enterance/page.tsx
+++ web/src/app/(..route)/customer/main/enterance/page.tsx
@@ -1,18 +1,39 @@
 'use client'
-import AchromaticButton from "@/app/ui/component/atom/button/achromatic-button";
+
 import { Card, CardContent, CardFooter, CardHeader } from "@/app/ui/component/molecule/card/card";
 import { useSearchParams } from "next/navigation";
+import { customerCategories, enterpriseCategories } from "./categories";
 
 export default function Home() {
   //To-Do: userSearchParam 이용해서 isIndividual의 값에 따라 컴포넌트 내에서 다른 내용 보여주기
   const params = useSearchParams();
   const key = params.get('isIndividual');
-  let pageTitle: string;
+  let pageTitle;
+  let pageCategory;
   if(key==='true'){
     pageTitle = '개인 금융'
+    pageCategory = customerCategories
   }else{
     pageTitle = '기업 금융'
+    pageCategory = enterpriseCategories
   }
+
+  const Categories:JSX.Element[] = pageCategory.map((value)=>{
+    const Icon = value.icon;
+    return(
+      <>
+        <Card className="bg-hwachang-green text-white">
+          <CardHeader className="text-3xl">
+            <strong>{value.title}</strong>
+          </CardHeader>
+          <CardContent className="pt-8" />
+          <CardFooter className="items-end justify-end">
+            <Icon color="white" size={100}></Icon>
+          </CardFooter>
+        </Card>
+      </>
+    )
+  })
   
   return (
     <main>
@@ -26,53 +47,9 @@ export default function Home() {
       </div>
     
       <div className="grid gap-6 sm:grid-rows-2 lg:grid-rows-4 text-center">
-      <div className={`grid gap-6 grid-cols-2 text-center`}>
-      <Card>
-        <CardHeader>
-          예적금 업무
-        </CardHeader>
-        <CardContent>
-          <div className="flex justify-center items-center ">
-          <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Cow%20Face.png" alt="Cow Face" width="100" height="100" />
-          </div>
-        </CardContent>
-        <CardFooter className="justify-center"> 선택하기 </CardFooter>
-      </Card>
-      <Card>
-      <CardHeader>
-          대출 업무
-        </CardHeader>
-        <CardContent>
-          <div className="flex justify-center items-center ">
-          <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Hamster.png" alt="Hamster" width="100" height="100" />
-          </div>
-        </CardContent>
-        <CardFooter className="justify-center"> 선택하기 </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader>
-          ISA계좌 개설
-        </CardHeader>
-        <CardContent>
-          <div className="flex justify-center items-center ">
-          <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Rabbit%20Face.png" alt="Rabbit Face" width="100" height="100" />
-          </div>
-        </CardContent>
-        <CardFooter className="justify-center"> 선택하기 </CardFooter>
-      </Card>
-      <Card>
-      <CardHeader>
-          심심해 놀아줘
-        </CardHeader>
-        <CardContent>
-          <div className="flex justify-center items-center ">
-          <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Cat%20Face.png" alt="Cat Face" width="100" height="100" />
-          </div>
-        </CardContent>
-        <CardFooter className="justify-center"> 선택하기 </CardFooter>
-      </Card>   
-      </div>
-      <AchromaticButton>상담하러 갈래요!</AchromaticButton>
+        <div className={`grid gap-6 grid-cols-3 text-center`}>
+          {Categories}
+        </div>
       </div>
     </main>
   );
