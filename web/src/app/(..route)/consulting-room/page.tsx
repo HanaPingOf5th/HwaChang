fba840a132af93cba57d338c4f76aa945ec6d977
yoounyoungheon <iddyoon@gmail.com>
2024-10-25 17:45:02 +0900

    feat: video 버튼

--- web/src/app/(..route)/consulting-room/page.tsx
+++ web/src/app/(..route)/consulting-room/page.tsx
@@ -5,12 +5,15 @@ import { Card, CardContent, CardFooter} from "@/app/ui/component/molecule/card/c
 import { useSearchParams } from "next/navigation";
 import Banker from '@/app/utils/public/banker.png';
 import { NameTag } from "@/app/ui/component/atom/name-tag";
+import { AiOutlineAudio } from "react-icons/ai";
+import { IoShareSocialOutline, IoSettingsOutline, IoVideocamOutline  } from "react-icons/io5";
 
 export default function Home() {
   // ToDo: userSearchParam 이용해서 isIndividual의 값에 따라 컴포넌트 내에서 다른 내용 보여주기
   const params = useSearchParams();
   const key = params.get('isWait');
-  
+  const buttonStatus = key==='true'?'대기중':'상담 종료';
+
   return (
     <main>
       {
@@ -70,11 +73,11 @@ export default function Home() {
           </CardFooter>
         </Card>
           <div className="flex justify-center space-x-4">
-              <AchromaticButton>button</AchromaticButton>
-              <AchromaticButton>button</AchromaticButton>
-              <AchromaticButton className="bg-red-400 hover:bg-red-600">상담 종료</AchromaticButton>
-              <AchromaticButton>button</AchromaticButton>
-              <AchromaticButton>button</AchromaticButton>
+              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><AiOutlineAudio color="black" size={20}/></AchromaticButton>
+              <AchromaticButton className="rounded-full  bg-hwachang-gray2 hover:bg-hwachang-gray3"><IoVideocamOutline color="black" size={20}/></AchromaticButton>
+              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3 text-black">{buttonStatus}</AchromaticButton>
+              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><IoShareSocialOutline color="black" size={20}/></AchromaticButton>
+              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><IoSettingsOutline color="black" size={20}/></AchromaticButton>
             </div>
       </div>
     </main>
