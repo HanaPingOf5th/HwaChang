add4252493a332f9b28ac67d6f0026fc5ad4fff4
Sujin Lim <dkssudtnwls4794@naver.com>
2024-10-25 17:49:14 +0900

    feat: 행원 라우팅 페이지 설정

---
+++ web/src/app/(..route)/teller/entrance/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import TabMenu from "@/app/ui/component/molecule/tabmenu/tab";
+
+export default function EntrancePage() {
+  return (
+    <div className="p-5">
+      <TabMenu />
+    </div>
+  )
+}
