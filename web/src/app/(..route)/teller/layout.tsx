e2bf9fd720fa701cdd3a74bc4ae4ebbf1a6d2d96
Sujin Lim <dkssudtnwls4794@naver.com>
2024-10-25 10:17:08 +0900

    fix: 행원 정보 props 관리

--- web/src/app/(..route)/teller/layout.tsx
+++ web/src/app/(..route)/teller/layout.tsx
@@ -1,10 +1,18 @@
 import TellerNav from "@/app/ui/nav/teller-nav";
 
 export default function Layout({ children }: { children: React.ReactNode; }) {
+  const tellerInfo = {
+    name: '임수진',
+    position: '대리',
+    branch: '성수역점',
+    department: '개인 금융 (대출 상담)',
+    availability: '상담 가능',
+  };
+
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-1/4">
-        <TellerNav></TellerNav>
+        <TellerNav tellerInfo={tellerInfo}></TellerNav>
       </div>
       <div className="flex-grow">{children}</div>
     </div>
--- web/src/app/ui/nav/teller-nav.tsx
+++ web/src/app/ui/nav/teller-nav.tsx
@@ -6,10 +6,23 @@ import ProfileImg from "@/app/utils/public/lalalping.png";
 import { Card } from "../component/molecule/card/card";
 import TellerNavLinks from "./teller-nav-link";
 
-export default function TellerNav() {
+interface TellerInfo {
+  name: string;
+  position: string;
+  branch: string;
+  department: string;
+  availability: string;
+}
+
+interface TellerInfoProps {
+  tellerInfo: TellerInfo;
+}
+
+export default function TellerNav({ tellerInfo }: TellerInfoProps) {
+
   return (
     <div className="flex h-full flex-col bg-hwachang-darkgreen">
-      <div className="flex h-36 justify-center items-center">
+      <div className="flex h-32 justify-center items-center">
         {/* 로고 */}
         <Image src={Logo} alt="Logo" width={120} height={100} className="mt-10" />
       </div>
@@ -32,24 +45,22 @@ export default function TellerNav() {
           <div className="p-5 text-center">
             <div className="flex flex-col items-center mb-4">
               <div className="flex items-baseline space-x-2 p-2">
-                <p className="text-4xl font-semibold">임수진</p>
-                <p className="text-xl text-gray1">대리</p>
+                <p className="text-4xl font-semibold">{tellerInfo.name}</p>
+                <p className="text-xl text-gray1">{tellerInfo.position}</p>
               </div>
             </div>
 
-            <p className="text-hwachang-black text-lg">성수역점</p>
-            <p className="text-hwachang-black text-lg">개인 금융 (대출 상담)</p>
+            <p className="text-hwachang-black text-lg">{tellerInfo.branch}</p>
+            <p className="text-hwachang-black text-lg">{tellerInfo.department}</p>
 
             <div className="flex items-center justify-center space-x-2">
               <div className="w-3 h-3 bg-hwachang-active rounded-full"></div>
-              <p className="text-hwachang-black text-lg">상담 가능</p>
+              <p className="text-hwachang-black text-lg">{tellerInfo.availability}</p>
             </div>
 
-            <div className="mt-6">
-              <AchromaticButton className="bg-hwachang-green rounded-3xl font-medium text-xl w-full py-9 shadow-lg">
-                상태 바꾸러 가기
-              </AchromaticButton>
-            </div>
+            <AchromaticButton className="bg-hwachang-green rounded-3xl font-medium text-xl w-full py-9 shadow-lg">
+              상태 바꾸러 가기
+            </AchromaticButton>
           </div>
         </Card>
       </div>
