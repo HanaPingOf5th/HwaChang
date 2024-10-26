3834739629b7e8c84fda3e50968bb823f3360665
DongEun <pobi555@naver.com>
2024-10-26 16:19:43 +0900

    feat: 고객 사이드 바 UI 구현

--- web/src/app/ui/nav/nav.tsx
+++ web/src/app/ui/nav/nav.tsx
@@ -1,18 +1,30 @@
-'use client'
-import AchromaticButton from "../component/atom/button/achromatic-button";
+"use client";
 import NavLinks from "./nav-link";
+import Image from "next/image";
+import Logo from "@/app/utils/public/Logo.png";
+import People from "@/app/utils/public/People.png";
 
 export default function Nav() {
   return (
-    <div className="flex h-full flex-col px-4 py-4 md:px-2">
-      <div className="mb-2 flex h-20 justify-start items-center rounded-md bg-emerald-300 p-4 md:h-30 border-2\">
-        <div className="text-white text-xl"> 화창</div>
+    <div className="flex h-full flex-col bg-hwachang-darkgreen">
+      <div className="flex h-32 justify-center items-center">
+        {/* 로고 */}
+        <Image src={Logo} alt="Logo" width={120} height={100} className="mt-10" />
       </div>
+
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavLinks/>
+        <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-emerald-10 md:block"></div>
-        <AchromaticButton className="bg-red-50 hover:bg-red-100 text-red-600">나가기</AchromaticButton>
+        <div className="flex items-center ml-5 mb-5">
+          {" "}
+          <Image src={People} alt="Profile" width={50} height={50} className="ml-4 mr-4 mb-5" />
+          <span className="text-white text-[17px] mr-8 mb-5">김동은님</span>{" "}
+          <button className="bg-hwachang-darkgreen text-white text-[15px] font-bold ml-auto mr-5 mb-5">
+            {" "}
+            로그아웃
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
