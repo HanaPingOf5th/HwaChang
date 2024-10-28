b5557f233d3dc4b6ecabb0f4a70987806614a69f
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-10-28 10:48:06 +0900

    fix: TellerNav바 tellerInfo 관리

--- web/src/app/(..route)/teller/layout.tsx
+++ web/src/app/(..route)/teller/layout.tsx
@@ -1,22 +1,11 @@
-"use client";
-
 import TellerNav from "@/app/ui/nav/teller-nav";
-import { usePathname } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode; }) {
-  const pathname = usePathname();
-  const tellerInfo = {
-    name: "임수진",
-    position: "대리",
-    branch: "성수역점",
-    department: "개인 금융 (대출 상담)",
-    availability: "상담 가능",
-  };
 
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-1/4">
-        <TellerNav tellerInfo={tellerInfo}></TellerNav>
+        <TellerNav />
       </div>
       <div className="flex-grow">{children}</div>
     </div>
