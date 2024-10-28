103586f0cc87acf9e2330df17be4c953d7d80e3c
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-10-28 10:32:28 +0900

    fix: 행원 Layout 간소화

--- web/src/app/(..route)/teller/layout.tsx
+++ web/src/app/(..route)/teller/layout.tsx
@@ -2,7 +2,6 @@
 
 import TellerNav from "@/app/ui/nav/teller-nav";
 import { usePathname } from "next/navigation";
-import EntrancePage from "./entrance/page";
 
 export default function Layout({ children }: { children: React.ReactNode; }) {
   const pathname = usePathname();
@@ -15,18 +14,11 @@ export default function Layout({ children }: { children: React.ReactNode; }) {
   };
 
   return (
-    <div>
-      {pathname === "/teller/entrance" ? (
-        <EntrancePage />
-      ) : (
-        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-          <div className="w-full flex-none md:w-1/4">
-            <TellerNav tellerInfo={tellerInfo}></TellerNav>
-          </div>
-          <div className="flex-grow">{children}</div>
-        </div>
-      )
-      }
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-1/4">
+        <TellerNav tellerInfo={tellerInfo}></TellerNav>
+      </div>
+      <div className="flex-grow">{children}</div>
     </div>
   );
 }
