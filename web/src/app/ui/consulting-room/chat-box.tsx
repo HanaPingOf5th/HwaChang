2ba7110e74966e5f65c88bda8b9c4ef1d54ee25d
yoounyoungheon <iddyoon@gmail.com>
2024-10-29 01:05:44 +0900

    feat: 채팅 시 자동 줄바꿈 기능 추가

--- web/src/app/ui/consulting-room/chat-box.tsx
+++ web/src/app/ui/consulting-room/chat-box.tsx
@@ -1,20 +1,28 @@
 export function MyChat({ chat }: { chat: string }) {
+  const formattedChat = chat.length > 15 ? chat.match(/.{1,15}/g)?.join('\n') : chat;
+
   return (
     <>
       <div className="flex justify-end mb-2">
-        <div className="bg-emerald-50 p-4 rounded-md shadow-md">{chat}</div>
+        <div className="bg-emerald-50 p-4 rounded-md shadow-md whitespace-pre-wrap">
+          {formattedChat}
+        </div>
       </div>
     </>
   );
 }
 
 export function OtherChat({ name, chat }: { name: string; chat: string }) {
+  const formattedChat = chat.length > 15 ? chat.match(/.{1,15}/g)?.join('\n') : chat;
+
   return (
     <>
       <div className="flex justify-start mb-2">
         <div className="flex flex-col">
           <div className="text-sm text-gray-600 mb-1">{name}</div>
-          <div className="bg-emerald-50 p-4 rounded-md shadow-md">{chat}</div>
+          <div className="bg-emerald-50 p-4 rounded-md shadow-md whitespace-pre-wrap">
+            {formattedChat}
+          </div>
         </div>
       </div>
     </>
