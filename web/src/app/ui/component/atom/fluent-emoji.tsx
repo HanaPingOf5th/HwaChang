aa6fac2c3c65b40adff8088ba098a5db7ab000d9
함형주 <hhj01hsh9901@korea.ac.kr>
2024-10-28 11:37:32 +0900

    컨설팅 룸 대기인원창, 메인페이지 약간 수정

--- web/src/app/ui/component/atom/fluent-emoji.tsx
+++ web/src/app/ui/component/atom/fluent-emoji.tsx
@@ -9,3 +9,7 @@ export function HouseEmoji({width, heignt}:{width: number, heignt:number}){
 export function EnterPriseEmoji({width, heignt}:{width: number, heignt:number}){
     return <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Office%20Building.png" alt="Office Building" width={`${width}`} height={`${heignt}`} />
 }
+
+export function GestureXEmoji({width, heignt}:{width: number, heignt:number}){
+    return <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/People/Person%20Gesturing%20No.png" alt="Person Gesturing No" width={`${width}`} height={`${heignt}`} />
+}
