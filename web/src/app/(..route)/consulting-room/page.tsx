cae84403ec7898484a97ab0cea38ee40b9964ab7
yoounyoungheon <iddyoon@gmail.com>
2024-10-30 10:24:26 +0900

    refactor: consulting room에서 view 컴포넌트 분리

--- web/src/app/(..route)/consulting-room/page.tsx
+++ web/src/app/(..route)/consulting-room/page.tsx
@@ -13,6 +13,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Xemoji from "@/app/utils/public/Xemoji.svg";
 import { MatchingAlarm } from "@/app/ui/consulting-room/matching-alarm";
+import { MainView } from "./components/main-view";
 
 
 export default function Home() {
@@ -26,6 +27,8 @@ export default function Home() {
   }, [params]);
 
   const buttonStatus = key === 'true' ? '대기중' : '상담 종료';
+
+  const participants = [];
 
   return (
     <main>
@@ -75,8 +78,8 @@ export default function Home() {
             }
           </Dialog>
         </div>
-{/*삭제예정*/}
-        <Dialog>
+        <MainView isTop={false}/>
+        {/* <Dialog>
             <DialogTrigger asChild>
               <AchromaticButton className="bg-hwachang-brightgreen hover:bg-hwachang-lightgreen text-black">
                 (삭제 예정) 매칭 알림
@@ -85,54 +88,51 @@ export default function Home() {
             <DialogContent>
               <MatchingAlarm/>
             </DialogContent>    
-        </Dialog>
-{/*삭제예정*/}
+        </Dialog> */}
       </div>
-      : <Card className="grid gap-6 grid-cols-3 text-center px-3 py-3">
-          <Card>
-            <></>
-            <CardContent>
-              <div className="flex flex-col justify-center items-center">
-                <FineEmoji heignt={150} width={150}/>
-              </div>
-            </CardContent>
-          </Card>
-          <Card>
-            <></>
-            <CardContent>
-              <div className="flex flex-col justify-center items-center">
-                <FineEmoji heignt={150} width={150}/>
-              </div>
-            </CardContent>
-          </Card>
-          <Card>
-            <></>
-            <CardContent>
-              <div className="flex flex-col justify-center items-center">
-                <FineEmoji heignt={150} width={150}/>
-              </div>
-            </CardContent>
-          </Card>
-    </Card>
-      }
+      :
+      <div>
+        <Card className="grid gap-6 grid-cols-3 text-center px-3 py-3 h-52">
+          <MainView isTop={true}/>
+          <MainView isTop={true}/>
+          <MainView isTop={true}/>
+            {/* <Card>
+              <></>
+              <CardContent>
+                <div className="flex flex-col justify-center items-center">
+                  <FineEmoji heignt={150} width={150}/>
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <></>
+              <CardContent>
+                <div className="flex flex-col justify-center items-center">
+                  <FineEmoji heignt={150} width={150}/>
+                </div>
+              </CardContent>
+            </Card>
+            <Card>
+              <></>
+              <CardContent>
+                <div className="flex flex-col justify-center items-center">
+                  <FineEmoji heignt={150} width={150}/>
+                </div>
+              </CardContent>
+            </Card> */}
+      </Card>
       <div className="grid gap-6 grid-rows text-center pt-4">
-        {/* 은행원 화면 ToDo: 배경 이미지 대신 웹소켓으로 받은 실시간 데이터를 보여주기 */}
-        <Card className="relative bg-cover bg-center" style={{ backgroundImage: `url(${Banker.src})` }}>
-          <CardContent>
-            <div className="p-60"/>
-          </CardContent>
-          <CardFooter>
-            <NameTag name="이수민"/>
-          </CardFooter>
-        </Card>
-          <div className="flex justify-center space-x-4">
-              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><AiOutlineAudio color="black" size={20}/></div></AchromaticButton>
-              <AchromaticButton className="rounded-full  bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><IoVideocamOutline color="black" size={20}/></div></AchromaticButton>
-              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3 text-black"><div className="p-2">{buttonStatus}</div></AchromaticButton>
-              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><IoShareSocialOutline color="black" size={20}/></div></AchromaticButton>
-              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><IoSettingsOutline color="black" size={20}/></div></AchromaticButton>
-            </div>
+        <MainView/>
+        <div className="flex justify-center space-x-4">
+          <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><AiOutlineAudio color="black" size={20}/></div></AchromaticButton>
+          <AchromaticButton className="rounded-full  bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><IoVideocamOutline color="black" size={20}/></div></AchromaticButton>
+          <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3 text-black"><div className="p-2">{'대기중'}</div></AchromaticButton>
+          <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><IoShareSocialOutline color="black" size={20}/></div></AchromaticButton>
+          <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><IoSettingsOutline color="black" size={20}/></div></AchromaticButton>
+        </div>
       </div>
+    </div>
+    }
     </main>
   );
 }
