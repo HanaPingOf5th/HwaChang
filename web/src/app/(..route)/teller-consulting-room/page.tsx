87f1fb035b208ea00ba8a12f3ea30405b967f1fd
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-10-30 17:21:33 +0900

    feat: 상담 준비 페이지 UI

--- web/src/app/(..route)/teller-consulting-room/page.tsx
+++ web/src/app/(..route)/teller-consulting-room/page.tsx
@@ -34,60 +34,23 @@ export default function Home() {
       {
         key == 'true'
           ?
-          <div className="grid grid-row-1 gap-1 p-3">
-            <p className={`mb-6 text-4xl text-hwachang-green1`} >
-              <strong>상담 준비중</strong>
+          <div className="grid grid-row-1 gap-1 p-10">
+            <p className={`mb-6 text-4xl font-semibold text-hwachang-green1`} >
+              상담 준비중
             </p>
             <div className="flex justify-between space-x-2">
               <p className={`mb-6 text-2xl text-hwachang-green1 font-semibold`} >
-                손님을 기다리고 있습니다.
+                손님을 기다리고 있습니다...
               </p>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <AchromaticButton className="bg-hwachang-brightgreen hover:bg-hwachang-lightgreen text-black">
-                    대기현황 보기
-                  </AchromaticButton>
-                </DialogTrigger>
-                {
-                  isDialogMounted &&
-                  <DialogContent>
-                    <div className="flex flex-col items-center">
-                      <div className="w-full flex justify-end">
-                        <Image src={Xemoji} alt="xemoji" width={20} height={20} />
-                      </div>
-                      <div className="flex justify-center p-10">
-                        <GestureXEmoji width={200} heignt={200} />
-                      </div>
-                      <p className="text-center text-lg font-semibold">현재 접속량이 많아</p>
-                      <p className="text-center text-lg font-semibold text-green-600">상담 대기 중입니다.</p>
-                      <p className="text-center text-sm text-gray-500 pt-3">조금만 기다려 주세요.</p>
-                      <div className="w-full mt-6">
-                        <div className="flex items-center justify-center space-x-2">
-                          <p className="text-lg font-medium">대기 인원</p>
-                          <IoRefresh className="mt-1" width={16} height={16} color="gray" />
-                          <p className="text-lg font-semibold">154,721명</p>
-                        </div>
-                        <div className="flex items-center justify-center mt-4 text-sm text-gray-500">
-                          <HiInformationCircle className="mr-1" width={16} height={16} color="gray" />
-                          <p className="pt-5">화상 상담 전 채팅을 통해 상담할 내용을 적어주시면<br /> 빠른 상담이 가능합니다.</p>
-                        </div>
-                      </div>
-                    </div>
-                  </DialogContent>
-                }
-              </Dialog>
             </div>
             <MainView isTop={false} />
-            <Dialog>
-              <DialogTrigger asChild>
-                <AchromaticButton className="bg-hwachang-brightgreen hover:bg-hwachang-lightgreen text-black">
-                  (삭제 예정) 매칭 알림
-                </AchromaticButton>
-              </DialogTrigger>
-              <DialogContent>
-                <MatchingAlarm />
-              </DialogContent>
-            </Dialog>
+            <div className="flex justify-center space-x-4 mt-4">
+              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><AiOutlineAudio color="black" size={20} /></div></AchromaticButton>
+              <AchromaticButton className="rounded-full  bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><IoVideocamOutline color="black" size={20} /></div></AchromaticButton>
+              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3 text-black"><div className="p-2">{buttonStatus}</div></AchromaticButton>
+              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><IoShareSocialOutline color="black" size={20} /></div></AchromaticButton>
+              <AchromaticButton className="rounded-full bg-hwachang-gray2 hover:bg-hwachang-gray3"><div className="p-2"><IoSettingsOutline color="black" size={20} /></div></AchromaticButton>
+            </div>
           </div>
           :
           <div className="grid grid-cols-[3fr_7fr] gap-6 h-full">
