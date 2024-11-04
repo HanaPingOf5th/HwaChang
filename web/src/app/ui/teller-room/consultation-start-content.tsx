0c46069c15c0fd2c6f30f2ce307838e94a1252de
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-11-04 10:09:40 +0900

    feat: 상담 입장하기 버튼 클릭 시 상담 대기화면으로 연결

--- web/src/app/ui/teller-room/consultation-start-content.tsx
+++ web/src/app/ui/teller-room/consultation-start-content.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import React from "react";
 import AchromaticButton from "../component/atom/button/achromatic-button";
+import { useRouter } from "next/navigation";
 
 interface waitingCountProps {
   waitingCount: number;
 }
 
 export default function ConsultationStartContent({ waitingCount }: waitingCountProps) {
+  const router = useRouter();
+
+  const handleEntrance = () => {
+    router.push("/teller-consulting-room");
+  }
+
   return (
     <div className="flex flex-col items-center space-y-8 mt-[28vh]">
       <div className="text-hwachang-green1 font-bold text-5xl mb-6">
@@ -14,7 +23,9 @@ export default function ConsultationStartContent({ waitingCount }: waitingCountP
       <div className="text-hwachang-green1 text-2xl">
         현재 {waitingCount}명의 고객이 대기 중에 있습니다.
       </div>
-      <AchromaticButton className="bg-hwachang-darkgreen w-1/4 rounded-2xl py-8 text-xl">
+      <AchromaticButton
+        className="bg-hwachang-darkgreen w-1/4 rounded-2xl py-8 text-xl"
+        onClick={handleEntrance}>
         입장하기
       </AchromaticButton>
     </div>
