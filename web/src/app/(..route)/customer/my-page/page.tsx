187a8462131ea887ffddcb53dcc63e0414f5202f
DongEun <pobi555@naver.com>
2024-11-04 17:40:34 +0900

    fix: 상담 요약 페이지 전체 화면 css 수정

--- web/src/app/(..route)/customer/my-page/page.tsx
+++ web/src/app/(..route)/customer/my-page/page.tsx
@@ -14,27 +14,31 @@ import Image from "next/image";
 export default function Home() {
   const [isSummaryVisible, setIsSummaryVisible] = useState(false);
   const [isFullScreen, setIsFullScreen] = useState(false);
-  const [selectedRecord, setSelectedRecord] = useState< ConsultingRecord | null>(null);
+  const [selectedRecord, setSelectedRecord] = useState<ConsultingRecord | null>(null);
 
   const handleOpenSummary = (record: ConsultingRecord) => {
     setSelectedRecord(record);
     setIsSummaryVisible(true);
+    console.log(isFullScreen);
+    console.log(isSummaryVisible);
   };
 
   const handleCloseSummary = () => {
     setIsSummaryVisible(false);
     setIsFullScreen(false);
+    console.log(isFullScreen);
+    console.log(isSummaryVisible);
   };
 
   const handleToggleFullScreen = () => {
-    setIsFullScreen((prev) => !prev); //전체화면 토글
+    setIsFullScreen((prev) => !prev); // 전체화면 토글
+    console.log(isFullScreen);
+    console.log(isSummaryVisible);
   };
 
-  const RecordCards = records.map((value, index)=>{
-    return (
-      <main key={index}>
+  const RecordCards = records.map((value, index) => (
+    <main key={index}>
       <Card className="grid grid-cols-7 gap-3">
-
         <div className="flex items-center justify-center mb-2 mt-2 text-sm md:text-sm lg:text-xl">
           <Image
             src={value.image}
@@ -42,26 +46,37 @@ export default function Home() {
             className="object-cover w-10 h-10 rounded-full border-1 border-white shadow-lg"
           />
         </div>
-        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">{value.title}</div>
-        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">{value.consultant}</div>
-        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">{value.cartegoryType}</div>
-        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">{value.cartegory}</div>
-        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">{value.date}</div>
-        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl"><button onClick={() => handleOpenSummary(value)}>{'->'}</button></div>
+        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">
+          {value.title}
+        </div>
+        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">
+          {value.consultant}
+        </div>
+        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">
+          {value.cartegoryType}
+        </div>
+        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">
+          {value.cartegory}
+        </div>
+        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">
+          {value.date}
+        </div>
+        <div className="flex items-center justify-center text-sm md:text-sm lg:text-xl">
+          <button onClick={() => handleOpenSummary(value)}>{"->"}</button>
+        </div>
       </Card>
-      </main>
-    )
-  })
+    </main>
+  ));
 
-  function searchAction(prevState: FormState, formData:FormData){
-    const value = formData.get('search')
-    console.log(value,'가 검색되었습니다.')
-    return{
+  function searchAction(prevState: FormState, formData: FormData) {
+    const value = formData.get("search");
+    console.log(value, "가 검색되었습니다.");
+    return {
       isSuccess: true,
       isFailure: false,
-      message: '',
-      validationError: {}
-    }
+      message: "",
+      validationError: {},
+    };
   }
 
   return (
@@ -69,12 +84,16 @@ export default function Home() {
       <div className="mb-5">
         <Form id={"search-form"} action={searchAction} failMessageControl={"alert"}>
           <div className="grid grid-cols-[8fr_1fr] gap-2">
-            <FormTextInput id={"search"} placeholder={"화창 기록을 검색해보세요"} className="rounded-full bg-slate-100"/>
-            <FormSubmitButton label={"검색"}/>
+            <FormTextInput
+              id={"search"}
+              placeholder={"화창 기록을 검색해보세요"}
+              className="rounded-full bg-slate-100"
+            />
+            <FormSubmitButton label={"검색"} />
           </div>
         </Form>
-      </div>  
-      <DateSelector/>
+      </div>
+      <DateSelector />
 
       <div style={{ fontSize: "30px", fontWeight: "Bold" }}>화창기록</div>
       <div className="grid grid-rows-1 gap-3 text-hwachang-hwachanggray text-lg text-center">
@@ -86,37 +105,34 @@ export default function Home() {
           <div>카테고리</div>
           <div>화창 날짜</div>
           <div></div>
-         </Card>
+        </Card>
         {RecordCards}
       </div>
 
       <div className="flex-col w-full mt-5">
-      {isSummaryVisible && (
-        <div
-          className={`fixed top-0 right-0 h-full ${isFullScreen ? "w-screen" : "w-1/2"}
-                      bg-white shadow-lg transition-width duration-300 z-50 
-                      overflow-y-auto pt-10 ${isFullScreen ? "p-10" : "py-7 px-8"}`}
-        >
+        {isSummaryVisible && (
           <div
-            className={`fixed top-0 right-0 ${isFullScreen ? "w-screen" : "w-1/2"}
-                        bg-white p-2 flex justify-start gap-2 z-60`}
+            className={`fixed top-0 right-0 h-full ${isFullScreen ? "w-screen" : "w-1/2"}
+                      bg-white shadow-lg transition-width duration-300 z-50 
+                      overflow-y-auto pt-5 ${isFullScreen ? "p-10" : "py-7 px-8"}`}
           >
-            <button
-              onClick={handleCloseSummary}
-              className="p-1 border-none bg-transparent cursor-pointer text-sm"
-            >
-              &gt;&gt;
-            </button>
-            <button
-              onClick={handleToggleFullScreen}
-              className="p-1 border-none bg-transparent cursor-pointer text-sm"
-            >
-              &lt;&gt;
-            </button>
+            <div className="relative">
+              <button
+                onClick={handleCloseSummary}
+                className="p-1 border-none bg-transparent cursor-pointer text-sm"
+              >
+                &gt;&gt;
+              </button>
+              <button
+                onClick={handleToggleFullScreen}
+                className="absolute top-0 right-0 p-1 border-none bg-transparent cursor-pointer text-sm"
+              >
+                &lt;&gt;
+              </button>
+            </div>
+            <Summary record={selectedRecord as ConsultingRecord} />
           </div>
-          <Summary record={selectedRecord as ConsultingRecord} />
-        </div>
-      )}
+        )}
       </div>
     </div>
   );
