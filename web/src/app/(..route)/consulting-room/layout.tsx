49ef3b82429ef7d1093e7d850e0988d524f2aa14
yoounyoungheon <iddyoon@gmail.com>
2024-11-04 13:23:13 +0900

    fix: 상담 화면 줄였을 때 나타나는 UI 버그 변경

--- web/src/app/(..route)/consulting-room/layout.tsx
+++ web/src/app/(..route)/consulting-room/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }:{ children: React.ReactNode; }) {
   return (
     <div className="flex h-screen flex-row overflow-hidden">
       <div className="flex-grow md:overflow-y-auto p-6">{children}</div>
-      <div className="w-1/4">
+      <div className="w-2/4 md:w-2/5 lg:w-5/2">
         <ConsultingRoomSideBar/>
       </div>
     </div>
