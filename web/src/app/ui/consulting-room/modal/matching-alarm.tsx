9c5cc779772c6247406ba44d93b174c54873c4e8
yoounyoungheon <iddyoon@gmail.com>
2024-11-13 14:28:24 +0900

    refactor: dialog 메인 page에서 분리

--- web/src/app/ui/consulting-room/modal/matching-alarm.tsx
+++ web/src/app/ui/consulting-room/modal/matching-alarm.tsx
@@ -1,6 +1,7 @@
 import { HiMiniBellAlert } from "react-icons/hi2";
-import AchromaticButton from "../component/atom/button/achromatic-button";
+
 import Link from "next/link";
+import AchromaticButton from "../../component/atom/button/achromatic-button";
 export function MatchingAlarm(){
     return(
         <>
@@ -9,7 +10,7 @@ export function MatchingAlarm(){
                 <p className="text-center ">상담사와 매칭되었습니다.</p>
                 <p className="text-center mb-4">입장하시겠습니까?</p>
                 <div className="flex gap-8">
-                    <AchromaticButton className="bg-red-500 hover:bg-red-400">거절</AchromaticButton>          
+                    <Link href={"/customer/main"}><AchromaticButton className="bg-red-500 hover:bg-red-400">거절</AchromaticButton></Link>         
                     <Link href={"/consulting-room"}><AchromaticButton >수락</AchromaticButton></Link>
                 </div>            
             </div>
