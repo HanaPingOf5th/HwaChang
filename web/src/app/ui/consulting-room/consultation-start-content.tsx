d05b08e0795a0d8ff7b425adb7f0c516842c6978
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-11-20 00:38:13 +0900

    feat: 행원 상담 대기실 표시

--- web/src/app/ui/consulting-room/consultation-start-content.tsx
+++ web/src/app/ui/consulting-room/consultation-start-content.tsx
@@ -11,7 +11,7 @@ export default function ConsultationStartContent({ waitingCount }: waitingCountP
   const router = useRouter();
 
   const handleEntrance = () => {
-    router.push("/teller-consulting-room");
+    router.push("/teller-consulting-room?isWait=true");
   }
 
   return (
