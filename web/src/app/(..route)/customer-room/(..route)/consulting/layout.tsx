1a29c08c22280044a3c9b8eb19a92d17789c2a10
yoounyoungheon <iddyoon@gmail.com>
2024-12-17 20:07:21 +0900

    fix: customer-room layout 분리

--- web/src/app/(..route)/customer-room/(..route)/consulting/layout.tsx
+++ web/src/app/(..route)/customer-room/(..route)/consulting/layout.tsx
@@ -1,11 +1,13 @@
-import CustomerConsultingRoomSideBar from "@/app/ui/consulting-room/customer-consulting-room-side-bar";
+import CustomerWaitingChatting from "@/app/ui/consulting-room/customer/customer-waiting-chatting";
 
 export default function Layout({ children }:{ children: React.ReactNode; }) {
   return (
     <div className="flex h-screen flex-row overflow-hidden">
       <div className="w-[70%] overflow-y-auto">{children}</div>
       <div className="w-[30%] overflow-x-auto bg-hwachang-darkgreen">
-        <CustomerConsultingRoomSideBar/>
+        <div className="grid grid-cols-1 gap-10 h-full p-7">
+          <CustomerWaitingChatting/>
+        </div>
       </div>
     </div>
   );
