3bb5e34000522702f327d3a407856de9ac642134
yoounyoungheon <iddyoon@gmail.com>
2024-12-19 23:42:43 +0900

    feat: 행원 상담 중 문서 검색 기능 구현

---
+++ web/src/app/business/categoty/category.service.ts
@@ -0,0 +1,19 @@
+'use server'
+
+import { APIResponseType, instance } from "@/app/utils/http";
+import { API_PATH } from "@/app/utils/http/api-query";
+
+export interface Category{
+  categoryId: string,
+  categoryName: string
+}
+
+export async function getCategories():Promise<APIResponseType>{
+  const response = await instance.get(`${API_PATH}/category`)
+  
+  return {
+    isSuccess: true,
+    isFailure: true,
+    data: response.data as Category
+  }
+}
